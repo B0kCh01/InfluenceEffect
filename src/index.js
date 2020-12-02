4a6e70aa468edaf0aa2107f74b5ccf9a6307ca94
b0kch01 <nath49ers@gmail.com>
2020-12-01 23:37:54 -0800

    Barebones finished

--- src/index.js
+++ src/index.js
@@ -1,6 +1,57 @@
 import "normalize.css";
 import "aos/dist/aos.css";
+import "./main.scss";
 
+import "@fortawesome/fontawesome-free/js/all";
 import AOS from "aos";
 
-AOS.init();
+AOS.init({
+    duration: 1000,
+    once: true,
+});
+
+import VanillaTilt from "vanilla-tilt";
+const cards = document.querySelectorAll("card");
+VanillaTilt.init(cards, {
+    glare: true,
+    scale: 1.1
+});
+
+
+// Splashscreen animations
+setTimeout(function() {
+    document.querySelector(".splash").className = "splash hide";
+    setTimeout(function() {
+        document.querySelector(".splash").remove();
+    }, 1000);
+}, 2000);
+
+
+// Simluate Clicking to pause youtube video
+function click(el, etype){
+    if (el.fireEvent) {
+        el.fireEvent('on' + etype);
+    } else {
+        var evObj = document.createEvent('Events');
+        evObj.initEvent(etype, true, false);
+        el.dispatchEvent(evObj);
+    }
+}
+
+// Modals
+
+let boxes = ["slides", "ad"];
+for (const id of boxes) {
+    const closeSlides = document.querySelector("#close-" + id);
+    if (closeSlides) closeSlides.addEventListener("click", function() {
+        document.querySelector("#" + id).className = "modal hide";
+        setTimeout(function() {
+            document.querySelector(".video").src += '';
+        }, 500);
+    });
+    
+    const showSlides = document.querySelector("#show-" + id);
+    if (showSlides) showSlides.addEventListener("click", function() {
+        document.querySelector("#" + id).className = "modal";
+    });
+}
